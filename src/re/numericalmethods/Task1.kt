e6ef3bd4de6f89c75c885b0641e0b9ec777e26a8
ongoni <ongoni.fg@gmail.com>
2017-09-25 21:51:00 +0400

    code refactor

--- src/re/numericalmethods/Task1.kt
+++ src/re/numericalmethods/Task1.kt
@@ -2,9 +2,9 @@ package re.numericalmethods
 
 class Task1 {
 
-    fun f(x: Double) = (1.0 / Math.sqrt(x)) * Math.sin(Math.sqrt(x))
+    private fun f(x: Double) = (1.0 / Math.sqrt(x)) * Math.sin(Math.sqrt(x))
 
-    fun sum(x: Double, e: Double) : Double {
+    private fun sum(x: Double, e: Double) : Double {
         var current = 1.0
         var result = 1.0
         var k = 0
@@ -18,47 +18,59 @@ class Task1 {
         return result - current
     }
 
-    fun right(f: Double, fnext: Double, h: Double) : Double = (fnext - f) / h
+    private fun rightDerivative(f: Double, fnext: Double, h: Double) : Double = (fnext - f) / h
 
-    fun left(fprev: Double, f: Double, h: Double) : Double = (f - fprev) / h
+    private fun leftDerivative(fprev: Double, f: Double, h: Double) : Double = (f - fprev) / h
 
-    fun central(fprev: Double, fnext: Double, h: Double) : Double = (fnext - fprev) / (2 * h)
+    private fun centralDerivative(fprev: Double, fnext: Double, h: Double) : Double = (fnext - fprev) / (2 * h)
+
+    fun printAsTable(values: MutableMap<Double, MutableList<Double>>) {
+        println("x\t\t f(x)\t\t sum(x)\t\t f+(x)\t\t f-(x)\t\t f+-(x)")
+    }
+
+    fun fillFuncValuesMap(a: Double, b: Double, h: Double, e: Double) : MutableMap<Double, MutableList<Double>> {
+        val funcValues: MutableMap<Double, MutableList<Double>> = mutableMapOf()
+        var x = a
+
+        while (x <= b) {
+            funcValues.put(x, mutableListOf())
+            funcValues[x]?.add(this.sum(x, e))
+            funcValues[x]?.add(this.f(x))
+
+            x += h
+        }
+
+        x = a + h
+        while (x < b) {
+            funcValues[x]?.add(this.rightDerivative(
+                    funcValues[x]!![0],
+                    funcValues[x + h]!![0],
+                    h
+            ))
+            funcValues[x]?.add(this.leftDerivative(
+                    funcValues[x - h]!![0],
+                    funcValues[x]!![0],
+                    h
+            ))
+            funcValues[x]?.add(this.centralDerivative(
+                    funcValues[x - h]!![0],
+                    funcValues[x + h]!![0],
+                    h
+            ))
+        }
+
+        return funcValues
+    }
 
 }
 
 fun main(args: Array<String>) {
-    val a = 0.0; val b = 1.7; val h = 0.1
-    val e = 0.0001
-    var x = a
-
     val task = Task1()
-    val funcValues: MutableMap<Double, MutableList<Double>> = mutableMapOf()
-
-    while (x <= b) {
-        funcValues.put(x, mutableListOf())
-        funcValues[x]?.add(task.sum(x, e))
-        funcValues[x]?.add(task.f(x))
-
-        x += h
-    }
-
-    x = a + h
-    while (x < b) {
-        funcValues[x]?.add(task.right(
-                funcValues[x]!![0],
-                funcValues[x + h]!![0],
-                h
-        ))
-        funcValues[x]?.add(task.left(
-                funcValues[x - h]!![0],
-                funcValues[x]!![0],
-                h
-        ))
-        funcValues[x]?.add(task.central(
-                funcValues[x - h]!![0],
-                funcValues[x + h]!![0],
-                h
-        ))
-    }
+    val values = task.fillFuncValuesMap(
+            a = 0.0,
+            b = 1.7,
+            h = 0.1,
+            e = 0.0001
+    )
 
 }
