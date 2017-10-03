3b2936d4b286f71420b35f2b328c1dc722550c43
ongoni <ongoni.fg@gmail.com>
2017-10-03 11:34:36 +0400

    added task 2

--- src/re/numericalmethods/Main.kt
+++ src/re/numericalmethods/Main.kt
@@ -5,14 +5,17 @@ class Main {
     companion object {
         @JvmStatic
         fun main(args: Array<String>) {
-            val task = Task1()
-            val values = task.fillFuncValuesMap(
-                    a = 0.0,
-                    b = 1.7,
-                    h = 0.1,
-                    e = 0.0001
-            )
-            task.printAsTable(values)
+//            val task = Task1()
+//            val values = task.fillFuncValuesMap(
+//                    a = 0.0,
+//                    b = 1.7,
+//                    h = 0.1,
+//                    e = 0.0001
+//            )
+//            task.printAsTable(values)
+
+            val task2 = Task2()
+            task2.run()
         }
     }
 
--- src/re/numericalmethods/Task2.kt
+++ src/re/numericalmethods/Task2.kt
@@ -4,12 +4,75 @@ class Task2 : DoubleValuesUtils {
 
     private fun f(x: Double) = Math.exp(2 * x) - 2 * Math.sin(x)
 
+    private fun rightRectangles(a: Double, n: Int, h: Double) : Double {
+        var result = 0.0
+        var x: Double
+
+        (1..n).forEach {
+            x = a + it * h
+            result += h * f(x)
+        }
+
+        return result
+    }
+
+    private fun leftRectangles(a: Double, n: Int, h: Double) : Double {
+        var result = 0.0
+        var x: Double
+
+        (0 until n).forEach {
+            x = a + it * h
+            result += h * f(x)
+        }
+
+        return result
+    }
+
+    private fun centralRectangles(a: Double, n: Int, h: Double) : Double {
+        var result = 0.0
+        var x: Double
+
+        (0 until n).forEach {
+            x = a + it * h
+            result += h * f(x + h / 2)
+        }
+
+        return result
+    }
+
+    private fun trapeze(a: Double, b: Double, n: Int, h: Double): Double {
+        var result = (h / 2) * (f(a) + f(b))
+        var x: Double
+
+        (1 until n).forEach {
+            x = a + it * h
+            result += h * f(x + h / 2)
+        }
+
+        return result
+    }
+
+    private fun simpson(a: Double, b: Double, n: Int, h: Double) : Double {
+        val m = n / 2
+
+        return (h / 3) * (
+                f(a) + f(b)
+                + 4 * (1 until 2 * m step 2).map { f(a + it * h) }.sum()
+                + 2 * (2..2 * m - 2 step 2).map { f(a + it * h) }.sum()
+                )
+    }
+
     fun run() {
-        val a = - Math.PI / 2; val b = 0
         print("enter n: ")
         val n = readLine()!!.toInt()
+        val a = -Math.PI / 2; val b = 0.0
+        val h = (b - a) / n
 
-        val h = getCorrectDouble((b - a) / n, 10)
+        println("right rectangles - " + rightRectangles(a, n, h))
+        println("left rectangles - " + leftRectangles(a, n, h))
+        println("central rectangles - " + centralRectangles(a, n, h))
+        println("trapeze - " + trapeze(a, b, n, h))
+        println("Simpson's formula - " + simpson(a, b, n, h))
     }
 
 }
