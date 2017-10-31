ffb33846972f962c5cc4bca392469db0bf01aef7
ongoni <ongoni.fg@gmail.com>
2017-10-31 11:36:37 +0400

    added task 3 (not complete)

--- src/re/numericalmethods/Main.kt
+++ src/re/numericalmethods/Main.kt
@@ -5,13 +5,13 @@ class Main {
     companion object {
         @JvmStatic
         fun main(args: Array<String>) {
-            println("Task 1:")
-            val task1 = Task1()
-            task1.run()
-
-            println("\nTask 2:")
-            val task2 = Task2()
-            task2.run()
+//            println("Task 1:")
+//            val task1 = Task1()
+//            task1.run()
+//
+//            println("\nTask 2:")
+//            val task2 = Task2()
+//            task2.run()
 
             println("\nTask 3:")
             val task3 = Task3()
--- src/re/numericalmethods/Task3.kt
+++ src/re/numericalmethods/Task3.kt
@@ -1,8 +1,50 @@
 package re.numericalmethods
 
-class Task3 {
+class Task3: DoubleValuesUtils {
+
+    private fun f(x: Double) = x * Math.cos(x)
+
+    private fun linearInterpolation(x: Double, prevX: Double, nextX: Double, prevF: Double, nextF: Double) =
+            ((x - nextX) / (prevX - nextX)) * prevF + ((x - prevX) / (nextX - prevX)) * nextF
+
+    private fun findPair(value: Double, values: MutableMap<Double, Double>) =
+        Pair(
+                values.keys.first { x -> x <= value },
+                values.keys.first { x -> x > value }
+        )
+
+    private fun firstMethod(a: Double, b: Double, n: Int) {
+        val functionValues = mutableMapOf<Double, Double>()
+        val h = (b - a) / n
+
+        var arg: Double
+        (0..50).forEach {
+            arg = getCorrectDouble(it * Math.PI / 13, 8)
+            functionValues.put(arg, getCorrectDouble(f(arg), 8))
+        }
+
+        var pair: Pair<Double, Double>
+        (0..n).forEach {
+            arg = getCorrectDouble(a + it * h, 8)
+            pair = findPair(arg, functionValues)
+            println("L($arg) = ${linearInterpolation(
+                    arg,
+                    pair.first,
+                    pair.second,
+                    functionValues[pair.first]!!,
+                    functionValues[pair.second]!!)
+            }")
+        }
+    }
 
     fun run() {
+        print("enter n: ")
+        val n = readLine()!!.toInt()
+//        print("enter N: ")
+//        val N = readLine()!!.toInt()
+        val a = 0.0; val b = 2 * Math.PI
+
+        firstMethod(a, b, n)
 
     }
 
